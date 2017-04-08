7ae6e197e653efcf22be1f5fd6a6ab1a1fbd426f
Iwakami <iwakami@users.noreply.github.com>
2017-04-08 19:46:43 -0400

    Programmed Total Calculations (words, characters, morae)

--- public/scripts/views/create.js
+++ public/scripts/views/create.js
@@ -1,59 +1,41 @@
-// // GET_USER.then(console.log)
-document.querySelector('input[type=submit]').addEventListener('click', () => {
-  let content = document.querySelector('textarea[name=content]').value;
+let getPassageTotals = (segmentsString) => {
+  let words = segmentsString.split("\n");
 
-  fetch('//web.ics.purdue.edu/~peter438/rfs/api.php/segment', {
-    method: 'POST',
-    body: JSON.stringify({
-      text: content
+  let totalWords = 0;
+  let totalCharacters = 0;
+  let totalMorae = 0;
+  words.forEach((word) => {
+    if (!word.includes("記号") && word !== "EOS" && word !== "" && !word.startsWith("...")) {
+      totalWords++;
+      let wordParts = word.split("\t");
+      totalCharacters += wordParts[0].length;
+      totalMorae += wordParts[1].length;
+
+    }
+  });
+
+  return {
+    words: totalWords,
+    characters: totalCharacters,
+    morae: totalMorae,
+  }
+};
+
+document.querySelector("input[type=submit]").addEventListener("click", () => {
+  let title = document.querySelector("input[name=title]").value;
+  let passage = document.querySelector("textarea[name=content]").value;
+  // let cleanPassage = passage.replace(/[\s\.\。\,\、\!\！\?\？\「\」\(\（\）\)\'\[\]\;\；\:\：\"\”\・]/gi, "");
+  let isJapanese = document.querySelector("input[type=checkbox]").checked;
+  // // GET_USER.then(console.log)
+  fetch("//web.ics.purdue.edu/~peter438/rfs/api.php/segment", {
+      method: "POST",
+      body: JSON.stringify({
+        text: passage
+      })
     })
-  })
     .then(response => response.json())
-    .then(console.log);
+    .then(json => {
+      let totals = getPassageTotals(json.segments);
+      console.log(totals);
+    });
 });
-
-// // t = title
-// var t = $_POST["title"]
-// var $txt = $t.".txt"
-// // c = content
-// var $c = $_POST["content"]
-// // ic = invalid characters
-// var $ic = array("\t",".","。"," ","　",",","、","!","！","?","？",";","；",":","：","\"","”","「","」","・","(","（","）",")","'","[","]","\n","\r\n","\r"," "," ")
-// // nic = no invalid characters
-// $nic = str_replace($ic, "", $c)
-// $niclen = mb_strlen($nic)
-// // jt = Japanese Text
-// $jt = $_POST["jt"]
-// // jchrc = Japanese Character Count
-// $jchrc = 0
-// // Mora Count
-// $mc = 0
-
-// file_put_contents($txt,$c)
-
-// echo "Have your readers use your title (<b>".$t."</b>) to retreive this reading passage.<br><br>"
-
-// echo "Total Character Count: <b>".$niclen."</b><br>"
-// if ($jt == "jt"){
-// 	$c = segment_jtext($c)
-// 	$lines = explode("\n",$c)
-// 	foreach($lines as $line){
-// 		if(strpos($line, "記号") > 0 || $line == "EOS" || $line == ""){
-// 			continue
-// 		}
-// 		else{
-// 			$jchrc += 1
-// 			// echo $line . "<br>"
-// 			$readings = explode("\t",$line)
-// 			$mc += mb_strlen($readings[1])
-// 		}
-// 	}
-// 	echo "Total Mora Count: <b>".$mc."</b><br>"
-// 	echo "Total Word Count: <b>".$jchrc."</b><br>(\"Words\" here are defined by a morphological analysis of the text using Purdue University's mecab service.)"
-// }
-// else {
-// 	echo "Total Word Count: <b>".str_word_count($c)."</b><br>"
-// }
-
-// echo "<br><b>Title: ".$t."</b><br>"
-// echo "<b><u>Text</u></b><br>".file_get_contents($txt)

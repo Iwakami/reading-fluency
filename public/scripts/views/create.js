b87b18d9a6ec1ff2427f3c65d967a01f98090c16
Iwakami <iwakami@users.noreply.github.com>
2017-04-08 20:43:39 -0400

    Saving passages to database works

--- public/scripts/views/create.js
+++ public/scripts/views/create.js
@@ -21,21 +21,40 @@ let getPassageTotals = (segmentsString) => {
   }
 };
 
+let getSegments = (passage) => {
+  return new Promise((resolve, reject) => {
+    fetch("//web.ics.purdue.edu/~peter438/rfs/api.php/segment", {
+        method: "POST",
+        body: JSON.stringify({
+          text: passage
+        })
+      })
+      .then(response => response.json())
+      .then(json => {
+        resolve(json.segments);
+      });
+  });
+};
+
 document.querySelector("input[type=submit]").addEventListener("click", () => {
   let title = document.querySelector("input[name=title]").value;
   let passage = document.querySelector("textarea[name=content]").value;
-  // let cleanPassage = passage.replace(/[\s\.\。\,\、\!\！\?\？\「\」\(\（\）\)\'\[\]\;\；\:\：\"\”\・]/gi, "");
   let isJapanese = document.querySelector("input[type=checkbox]").checked;
-  // // GET_USER.then(console.log)
-  fetch("//web.ics.purdue.edu/~peter438/rfs/api.php/segment", {
-      method: "POST",
-      body: JSON.stringify({
-        text: passage
-      })
-    })
-    .then(response => response.json())
-    .then(json => {
-      let totals = getPassageTotals(json.segments);
-      console.log(totals);
+
+  Promise.all([GET_USER, getSegments(passage)])
+    .then(([user, segments]) => {
+      let totals = getPassageTotals(segments);
+
+      let passagesReference = db.ref("passages");
+      let passageId = passagesReference.push().key;
+      let dbPassage = passagesReference.child(passageId);
+      dbPassage.set({
+        creatorId: user.uid,
+        created: Date.now(),
+        title,
+        passage,
+        isJapanese,
+        totals
+      });
     });
 });
